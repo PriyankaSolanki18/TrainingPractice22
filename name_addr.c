a18961004636dba8dde0ba34c1305f1041c4dbf5
PriyankaSolanki18 <contactharshithere@gmail.com>
2024-09-02 10:43:24 +0000

    Added .c, .i, .s and nameAddrOutput files

---
+++ name_addr.c
@@ -0,0 +1,15 @@
+/* This is an C program  for printing name and address of a user
+OWNER : Priyanka Solanki	DATE : 02-09-2024
+REVISION HISTORY: 
+				NAME :	DATE :	REASON :
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("My name is : Priyanka Solanki\n");
+	printf("My  address is : Ajmer, Rajasthan\n");
+	return EXIT_SUCCESS;
+}
+
